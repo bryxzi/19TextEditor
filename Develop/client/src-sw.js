961651a49dd6aedf16648d2a80193eb3fd986be1
Keyur Bisetty <keyur@Keyurs-MacBook-Pro.local>
2023-04-05 16:22:30 -0400

    using regular expression

--- Develop/client/src-sw.js
+++ Develop/client/src-sw.js
@@ -26,5 +26,17 @@ warmStrategyCache({
 
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
-// TODO: Implement asset caching
-registerRoute();
+registerRoute(
+  /\.(?:png|gif|jpg|jpeg|svg|css|js)$/,
+  new CacheFirst({
+    cacheName: 'asset-cache',
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+      new ExpirationPlugin({
+        maxAgeSeconds: 30 * 24 * 60 * 60,
+      }),
+    ],
+  })
+);
