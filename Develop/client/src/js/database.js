07fe304b3809d58f5a8ff2c62138b869527f5d9f
Keyur Bisetty <keyur@Keyurs-MacBook-Pro.local>
2023-04-05 15:33:28 -0400

    added database.js

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -1,21 +1,31 @@
 import { openDB } from 'idb';
 
 const initdb = async () =>
-  openDB('jate', 1, {
+  openDB('text-editor', 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
+      if (db.objectStoreNames.contains('documents')) {
+        console.log('text-editor database already exists');
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
+      db.createObjectStore('documents', { keyPath: 'id', autoIncrement: true });
+      console.log('text-editor database created');
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+// Add logic to a method that accepts some content and adds it to the database
+export const saveDocument = async (document) => {
+  const db = await initdb();
+  const tx = db.transaction('documents', 'readwrite');
+  const store = tx.objectStore('documents');
+  await store.add(document);
+  await tx.complete;
+};
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+// Add logic for a method that gets all the content from the database
+export const getDocuments = async () => {
+  const db = await initdb();
+  const tx = db.transaction('documents', 'readonly');
+  const store = tx.objectStore('documents');
+  return store.getAll();
+};
 
-initdb();
