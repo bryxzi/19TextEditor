c36d8f33f1081268e2b6eceeff289e4b64a23562
Keyur Bisetty <keyur@Keyurs-MacBook-Pro.local>
2023-04-05 16:28:35 -0400

    updated webpack

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -1,10 +1,8 @@
+const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
-const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { InjectManifest } = require('workbox-webpack-plugin');
-
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
 
 module.exports = () => {
   return {
@@ -15,15 +13,56 @@ module.exports = () => {
     },
     output: {
       filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
+      path: path.resolve(__dirname, 'dist'), 
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        title: 'JATE',  
+        template: './src/index.html', 
+      }),
+      new MiniCssExtractPlugin(),
+      new InjectManifest({
+        swSrc: './src/src-sw.js', 
+        swDest: 'sw.js', 
+      }),
+      new WebpackPwaManifest({
+        name: 'jate', 
+        short_name: 'jate', 
+        description: 'Text editor that can be installed from the web browser', 
+        background_color: '#01579b',
+        theme_color: '#7eb4e2',
+        start_url: '/', 
+        publicPath: '/', 
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),      
     ],
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i, 
+          use: [MiniCssExtractPlugin.loader, 'css-loader'], 
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i, 
+          type: 'asset/resource',
+        },
+        {
+          test: /\.m?js$/, 
+          exclude: /(node_modules|bower_components)/, 
+          use: { 
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
+        },
       ],
     },
   };
