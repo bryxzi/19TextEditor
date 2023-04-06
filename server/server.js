9d011a6c6eaf822effa0da7dca7fbaed74cb84de
Keyur Bisetty <keyur@Keyurs-MacBook-Pro.local>
2023-04-06 13:48:59 -0400

    tweak server

--- server/server.js
+++ server/server.js
@@ -1,9 +1,10 @@
 const express = require('express');
-
+const path = require('path'); // added path
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static('../client/dist'));
+// Updated the path 
+app.use(express.static(path.join(__dirname, '../client/dist')));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
